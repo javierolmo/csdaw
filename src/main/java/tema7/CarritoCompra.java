c57785ea8453a518dccf9c9ae4f64293f466aaf9
JaviOlmoI <44966721+JaviOlmoI@users.noreply.github.com>
2022-01-16 11:01:44 +0100

    Ejercicio8

---
+++ src/main/java/tema7/CarritoCompra.java
@@ -0,0 +1,33 @@
+package tema7;
+
+public class CarritoCompra {
+
+    private final int MAX_ARTICULOS = 100;
+
+    private Producto[] contenido;
+    private double total;
+
+    public CarritoCompra() {
+        total = 0;
+        contenido = new Producto[MAX_ARTICULOS];
+    }
+
+    public Producto[] getContenido() {
+        return contenido;
+    }
+
+    public String addProducto(Producto producto) {
+        for(int i=0; i< contenido.length; i++) {
+            if (contenido[i] == null) {
+                contenido[i] = producto;
+                total += producto.getPrecioTotal();
+                return "Producto añadido correctamente";
+            }
+        }
+        return "No hay más espacio en la cesta";
+    }
+
+    public double getTotal() {
+        return total;
+    }
+}
