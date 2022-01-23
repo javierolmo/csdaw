d9e6ff714855eeab37effbcd086ab07836c2e365
JaviOlmoI <44966721+JaviOlmoI@users.noreply.github.com>
2022-01-23 11:28:55 +0100

    Tema 8 ejercicios 4, 9, 10 y 11

---
+++ src/main/java/tema8/CuentaPlazo.java
@@ -0,0 +1,23 @@
+package tema8;
+
+import java.time.LocalDate;
+
+public final class CuentaPlazo extends CuentaCorriente {
+
+    private LocalDate fechaCreacion;
+
+    public CuentaPlazo(String iban) {
+        super(iban);
+        setComision(0, 0);
+        this.fechaCreacion = LocalDate.now();
+    }
+
+    @Override
+    public boolean retirar(float importe) {
+        if (importe <= getSaldo()) {
+            setSaldo(getSaldo() - importe);  //resta (importe+comision)
+            setContadorIngresos(0);
+            return true;
+        } else return false;
+    }
+}
