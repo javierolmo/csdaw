0b8d54fc5b35c1d0d79a6d95efff76fa93cce512
Javi <olmo.injerto.javier@gmail.com>
2022-03-05 08:46:08 +0100

    Ejercicio1

---
+++ src/main/java/csdaw/tema12/ejercicio1/Conversor.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package csdaw.tema12.ejercicio1;
+
+/**
+ *
+ * @author javi
+ */
+public class Conversor {
+    
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9/5) + 32;
+    }
+    
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5/9;
+    }
+    
+    
+    
+}
